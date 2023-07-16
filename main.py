00668136c08db3e66a2812c4f61470d6080c9228
rootVSA <rootvsa@ROOTVSA.com>
2023-07-16 09:25:11 -0400

    Primeira implementação da API

--- main.py
+++ main.py
@@ -1,20 +1,41 @@
 from classes.school import SchoolOperations
 from classes.driver import DriverOperations
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
-if __name__ == "__main__":
-    try:
-        number_schools = int(input("Digite um número de escolas para registrar no arquivo CSV (-1 para registrar todas as escolas do banco): "))
-        assert number_schools >= -1
-        type_school = int(input("\nDigite o tipo de escola:\n>Pública[0]\n>Privada[1]\n>Pública e Privada[2]\n: "))
-        assert type_school >= 0 and type_school <= 2
-    except AssertionError:
-        print("\nValor fora de intervalo.")
-        exit()
+app = FastAPI()
 
-    types_schools = ['Public','Private','Public Private']
+origins = [
+    "http://localhost:4200"
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# try:
+#     number_schools = int(input("Digite um número de escolas para registrar no arquivo CSV (-1 para registrar todas as escolas do banco): "))
+#     assert number_schools >= -1
+#     type_school = int(input("\nDigite o tipo de escola:\n>Pública[0]\n>Privada[1]\n>Pública e Privada[2]\n: "))
+#     assert type_school >= 0 and type_school <= 2
+# except AssertionError:
+#     print("\nValor fora de intervalo.")
+#     exit()
+
+class SearchInfo(BaseModel):
+    type_school: str
+    number_schools: int
+
+@app.post("/receber-opcao")
+async def executar_back(search_info: SearchInfo): 
     driver_chrome = DriverOperations()
     driver_chrome.start_driver()
 
     school = SchoolOperations(driver_chrome.driver)
-    school.search_school(number_schools,types_schools[type_school])
+    school.search_school(search_info.number_schools,search_info.type_school)
+    return {"message": "Ação executada com sucesso"}
